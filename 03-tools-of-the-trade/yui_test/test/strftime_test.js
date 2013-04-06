56258247951a7d7a5eccc1de7052b92325b00966
hiroo takaku <slgerkamp@gmail.com>
2013-04-06 16:15:38 +0900

    section03

---
+++ 03-tools-of-the-trade/yui_test/test/strftime_test.js
@@ -0,0 +1,68 @@
+YUI({
+	combine : true,
+	timeout : 10000
+}).use("node", "console", "test", function(Y){
+
+	// アサーション
+	var assert = Y.Assert;
+
+	// テストケース
+	var strftimeTestCase = new Y.Test.Case({
+
+		// テストケース名 指定しなければ自動生成
+		name : "Date.prototype.strftime Test",
+
+		// テスト前のテストテクスチャ作成
+		setUp : function(){
+			this.date = new Date(2009, 9, 2, 22, 14, 45);
+		},
+
+		// テスト後のテストテクスチャの後始末
+		tearDown : function(){
+			delete this.date;
+		},
+
+		"test %Y should return year" : function(){
+			var year = Date.formats.Y(this.date);
+
+			assert.isString(year);
+			assert.areEqual("2009",year);
+
+		},
+		"test %m should return month" : function(){
+			var month = Date.formats.m(this.date);
+
+			assert.isString(month);
+			assert.areEqual("10",month);
+
+		},
+		"test %d should return date" : function(){
+
+			assert.areEqual("02",Date.formats.d(this.date));
+
+		},
+		"test %y should return year as two digits" : function(){
+
+			assert.areEqual("09",Date.formats.y(this.date));
+
+		},
+
+		"test %F should act as %Y-%m-%d" : function(){
+
+			assert.areEqual("2009-10-02", this.date.strftime("%F"));
+
+		}
+
+	});
+
+	// コンソール作成
+	var r = new Y.Console({
+		newestOnTop : false,
+		style : 'block'
+	});
+
+	r.render("#testReport");
+	Y.Test.Runner.add(strftimeTestCase);
+	Y.Test.Runner.run();
+
+});
