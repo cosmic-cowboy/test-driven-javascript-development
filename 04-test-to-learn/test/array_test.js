181fc4fff272a2f9d9a351682796239459254410
hiroo takaku <slgerkamp@gmail.com>
2013-04-06 18:09:35 +0900

    list4-4

--- 04-test-to-learn/test/array_test.js
+++ 04-test-to-learn/test/array_test.js
@@ -1,7 +1,8 @@
 // 配列テスト
 TestCase("ArrayTest", {
 
-	// Array_prototype_spliceは配列を書き換えない
+	// list 4.1 Array.prototype.spliceは配列を書き換えないか
+	// list 4.2 Array.prototype.spliceは配列を書き換える
 	"test array splice should not modify array": function(){
 
 		var arr = [1, 2, 3, 4, 5];
@@ -10,13 +11,14 @@ TestCase("ArrayTest", {
 		assertEquals([1,2], arr);
 
 	},
-	// Array_prototype_spliceは部分削除後の配列を返すか
+	// list 4-3 Array.prototype.spliceは部分削除後の配列を返すか
+	// list 4-4 Array.prototype.spliceは削除した要素を返してくる
 	"test array splice should return modified array" : function(){
 
 		var arr = [1,2,3,4,5];
 		var result = arr.splice(2,3);
 
-		assertEquals(arr,result);
+		assertEquals([3,4,5],result);
 	}
 
 });
