6407cf26e9ebad632ff6b3a1cc05221ce9dbfbf5
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 11:07:39 +0900

    list4-5 add explanation

--- 04-test-to-learn/lib/benchmark.js
+++ 04-test-to-learn/lib/benchmark.js
@@ -7,10 +7,11 @@ function runBenchmark(name, test){
 	}
 
 	setTimeout(function(){
-		// スタート時間
+		// 1. new Date()する（テストスタート時間）
 		var start = new Date().getTime();
+		// 2. 計測対象のコードを実行する
 		test();
-		// 実行時間
+		// 3. new Date()する（終了：処理にかかった時間を算出）
 		var total = new Date().getTime() - start ;
 
 		var li = document.createElement("li");
