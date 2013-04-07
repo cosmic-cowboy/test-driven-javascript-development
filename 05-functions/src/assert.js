21f9d8d5a63e178e861c2a7e4755f530bac5e367
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 15:06:50 +0900

    list5-2

--- 05-functions/src/assert.js
+++ 05-functions/src/assert.js
@@ -1,5 +1,9 @@
-// 関数宣言
-function assert(message, expr){
+// 関数式, 無名関数が作られる
+// 使用法1. ほかに関数に渡す
+// 使用法2. 関数の戻り値に
+// 使用法3. 変数・オブジェクトのプロパティに代入
+
+var assert = function(message, expr){
 
 	if(!expr){
 		throw new Error(message);
@@ -8,7 +12,7 @@ function assert(message, expr){
 	assert.count++;
 
 	return true;
-}
+// 関数式には終端子としてセミコロンをつける
+};
 
-// 関数はオブジェクトなのでプロパティを持つことができる
 assert.count = 0;
