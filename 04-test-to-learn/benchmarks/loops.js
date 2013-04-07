f0a3c54c6c105ee77b12a5196a49195667e570bf
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 12:12:13 +0900

    list4-10

--- 04-test-to-learn/benchmarks/loops.js
+++ 04-test-to-learn/benchmarks/loops.js
@@ -1,4 +1,4 @@
-var loopLength = 500000;
+var loopLength = 100000;
 
 // ループ実行のために配列に値をセット
 var array = [];
@@ -7,79 +7,72 @@ for(var i = 0; i < loopLength; i++){
 	array[i] = "item" + i;
 }
 
-// test01
-function forLoop(){
-	for (var i = 0, item; i < array.length; i++) {
-		item = array[i];
+// benchmark.js benchmark(name, tests, iterations)
+benchmark("Loop performance", {
+
+	// test01
+	"for-loop" : function(){
+		for (var i = 0, item; i < array.length; i++) {
+			item = array[i];
+		}
+	},
+
+	// test02
+	"for-loop, cached length" : function(){
+		for (var i = 0, l = array.length, item; i < l; i++) {
+			item = array[i];
+		}
+	},
+
+	// test03
+	"for-loop, direct access" : function(){
+		for (var i = 0, item; (item = array[i]); i++) {
+
+		}
+	},
+
+	// test04
+	"while-loop" : function(){
+		var i = 0, item;
+
+		while(i < array.length){
+			item = array[i];
+			i++;
+		}
+	},
+
+	// test05
+	"while-loop, cached length" : function(){
+		var i = 0, l = array.length, item;
+
+		while(i < l){
+			item = array[i];
+			i++;
+		}
+	},
+
+	// test06
+	"reverse-while-loop" : function(){
+		var l = array.length, item;
+
+		while(l--){
+			item = array[l];
+		}
+	},
+
+	// test07
+	"double-reverse-while-loop" : function (){
+		var l = array.length, i = l, item;
+
+		while(i--){
+			item = array[l - i - 1];
+		}
 	}
-}
 
-// test02
-function forLoopCachedLength(){
-	for (var i = 0, l = array.length, item; i < l; i++) {
-		item = array[i];
-	}
-}
-
-// test03
-function forLoopDirectAccess(){
-	for (var i = 0, item; (item = array[i]); i++) {
-
-	}
-}
-
-// test04
-function whileLoop(){
-	var i = 0, item;
-
-	while(i < array.length){
-		item = array[i];
-		i++;
-	}
-}
-
-// test05
-function whileLoopCachedLength(){
-	var i = 0, l = array.length, item;
-
-	while(i < l){
-		item = array[i];
-		i++;
-	}
-}
-
-// test06
-function reverseWhileLoop(){
-	var l = array.length, item;
-
-	while(l--){
-		item = array[l];
-	}
-}
-
-// test07
-function doubleReverseWhileLoop(){
-	var l = array.length, i = l, item;
-
-	while(i--){
-		item = array[l - i - 1];
-	}
-}
+}, 1000);
 
 
-// テストを実行
-runBenchmark("for-loop", forLoop);
 
-runBenchmark("for-loop, cached length", forLoopCachedLength);
 
-runBenchmark("for-loop, direct access", forLoopDirectAccess);
-
-runBenchmark("while-loop", whileLoop);
-
-runBenchmark("while-loop, cached length", whileLoopCachedLength);
-
-runBenchmark("reverse-while-loop", reverseWhileLoop);
-
-runBenchmark("double-reverse-while-loop", doubleReverseWhileLoop);
 
 
