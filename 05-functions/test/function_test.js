7849a87c29f8d3e4be064e25203f5b099e22d1ef
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 18:01:06 +0900

    list5-10

--- 05-functions/test/function_test.js
+++ 05-functions/test/function_test.js
@@ -17,3 +17,16 @@ TestCase("Function Test",{
 	}
 
 });
+
+
+TestCase("FormalParametersArgumentsTest", {
+	"test dynamic relationship" : function(){
+		function modify(a,b){
+			b = 42;
+			arguments[0] = arguments[1];
+
+			return a;
+		}
+		assertEquals(42, modify(1,2));
+	}
+});
