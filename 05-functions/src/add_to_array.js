c84fa5683c308f6e7589e5623528e0548c8f6b22
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 17:21:49 +0900

    list5-9

--- 05-functions/src/add_to_array.js
+++ 05-functions/src/add_to_array.js
@@ -1,7 +1,10 @@
 // argumentsで配列メソッドを使う
 
 function addToArray(){
-	var targetArr = arguments[0];
+
+	// 文字列によるプロパティへのアクセス
+
+	var targetArr = arguments["0"];
 	var add = Array.prototype.slice.call(arguments, 1);
 
 	return targetArr.concat(add);
