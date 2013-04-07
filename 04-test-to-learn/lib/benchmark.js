2545df2fbdbc147a0fc896701bcc0cad72d94204
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 13:59:17 +0900

    list4-11

--- 04-test-to-learn/lib/benchmark.js
+++ 04-test-to-learn/lib/benchmark.js
@@ -1,5 +1,8 @@
 var benchmark = (function(){
 
+	// 時間を計測する
+	var times;
+
 	// Initialization create Ordered List
 	function init(name){
 		var heading = document.createElement("h2");
@@ -29,28 +32,74 @@ var benchmark = (function(){
 					if(!test.length){
 						while(l--){
 							test();
-						}						
+						}
 					} else {
 						test(1);
 					}
 
 					// test end 
 					var total = new Date().getTime() - start;
+					times[name] = total;
 
 					var li = document.createElement("li");
 					li.innerHTML = name + " : " + total + "ms(total), " + (total / iterations) + "ms(avg)";
 					view.appendChild(li);
 
 				}, 15);
-
 			}(label, tests[label]));
-		}	
+		}
+	}
+
+	function highlightExtremes(view){
+
+		// タイムアウトは他のすべてのタイマーのあとにキューイングされる
+		// すべてのテストが実行を終了し、
+		// timesオブジェクトに値がセットされていることが保証されている
+		setTimeout(function(){
+
+			var min = new Date().getTime();
+			var max = 0;
+			var fastest, slowest;
+
+			for(var label in times){
+				if(!times.hasOwnProperty(label)){
+					continue;
+				}
+
+				if(times[label] < min){
+					min = times[label];
+					fastest = label;
+				}
+
+				if(times[label] > max){
+					max = times[label];
+					slowest = label;
+				}
+			}
+
+			var lis = view.getElementsByTagName("li");
+			var fastRegexp = new RegExp("^" + fastest + " :");
+			var slowRegexp = new RegExp("^" + slowest + " :");
+
+			for (var i = 0; i < lis.length; i++) {
+				if(slowRegexp.test(lis[i].innerHTML)){
+					lis[i].style.color = '#c00';
+				}
+
+				if(fastRegexp.test(lis[i].innerHTML)){
+					lis[i].style.color = '#0c0';
+				}
+			}
+		}, 15);
+
 	}
 
 	function benchmark(name, tests, iterations){
 		iterations = iterations || 1000;
+		times = {};
 		var view = init(name);
 		runTests(tests, view, iterations);
+		highlightExtremes(view);
 	}
 
 	return benchmark;
