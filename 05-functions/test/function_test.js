cf8957332b5fd5443e7578802289e0e7adfde2fa
hiroo takaku <slgerkamp@gmail.com>
2013-04-07 19:15:22 +0900

    list5-14

--- 05-functions/test/function_test.js
+++ 05-functions/test/function_test.js
@@ -66,3 +66,19 @@ TestCase("FormalParametersArgumentsTest", {
 
 
 });
+
+var global = this;
+
+TestCase("GlobalObjectTest", {
+	"test window should be global object" : function(){
+		assertSame(global, window);
+		assertSame(global.window, window);
+		assertSame(window.window, window);
+	}
+})
+
+
+
+
+
+
