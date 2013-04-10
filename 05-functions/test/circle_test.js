5f62fbc722d398ce8900974b6e92b2e9d2a87af4
hiroo takaku <slgerkamp@gmail.com>
2013-04-11 00:18:10 +0900

    list5-24

--- 05-functions/test/circle_test.js
+++ 05-functions/test/circle_test.js
@@ -1,4 +1,3 @@
-// list5-23
 
 var circle = {
 
@@ -10,7 +9,19 @@ var circle = {
 };
 
 TestCase("CircleTest", {
+
+	// list5-23
 	"test should implicitly bind to object" : function(){
 		assertEquals(12, circle.diameter());
+	},
+
+	// list5-24
+	"test implicit binding to the global object" : function(){
+		var myDiameter = circle.diameter;
+		assertNaN(myDiameter());
+
+		radius = 2;
+		assertEquals(4, myDiameter());
 	}
 });
+
