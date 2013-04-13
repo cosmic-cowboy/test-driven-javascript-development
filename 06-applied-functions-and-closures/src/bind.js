b5c1f140421bab2d2af36905904b14ee13742437
hiroo takaku <slgerkamp@gmail.com>
2013-04-13 13:31:28 +0900

    list6-6

--- 06-applied-functions-and-closures/src/bind.js
+++ 06-applied-functions-and-closures/src/bind.js
@@ -1,11 +1,14 @@
-// list6-4 bindの実装
+// list6-6 引数のバインドもサポートするbind
 
 if(!Function.prototype.bind) {
 	Function.prototype.bind = function(thisObj) {
 		var target = this;
+		var args   = Array.prototype.slice.call(arguments, 1);
 
 		return function(){
-			return target.apply(thisObj, arguments);
+			var received = Array.prototype.slice.call(arguments);
+
+			return target.apply(thisObj, args.concat(received));
 		};
 	};
 }
