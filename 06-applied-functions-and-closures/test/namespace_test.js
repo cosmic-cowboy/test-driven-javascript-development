8cefdce90431c892f940d862a71e2a85c52cdcc5
hiroo takaku <slgerkamp@gmail.com>
2013-04-13 16:38:26 +0900

    list6-15

---
+++ 06-applied-functions-and-closures/test/namespace_test.js
@@ -0,0 +1,27 @@
+// list6-15 namespace関数
+TestCase("NamespaceTest", {
+	tearDown: function () {
+		delete tddjs.nstest;
+	},
+
+	"test should create non-existent object": function () {
+		tddjs.namespace("nstest");
+		assertObject(tddjs.nstest);
+	},
+
+	"test should not overwrite existing object": function () {
+		tddjs.nstest = { nested:{}};
+		var result = tddjs.namespace("nstest.nested");
+
+		assertSame(tddjs.nstest.nested, result);
+	},
+
+	"test only create missing parts": function(){
+		var existing = {};
+		tddjs.nstest = {nested : {existing:existing}};
+		var result = tddjs.namespace("nstest.nested.ui");
+
+		assertSame(existing, tddjs.nstest.nested.existing);
+		assertObject(tddjs.nstest.nested.ui);
+	}
+});
