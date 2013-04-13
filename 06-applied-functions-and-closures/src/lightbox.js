7ecd8ffd8082cdda4cdf7cebf1b86aca6026c3ca
hiroo takaku <slgerkamp@gmail.com>
2013-04-13 13:12:34 +0900

    list6-1

---
+++ 06-applied-functions-and-closures/src/lightbox.js
@@ -0,0 +1,32 @@
+// list6-1
+var lightbox = {
+	open: function () {
+		ajax.loadFragment(this.url, {
+			target: this.close
+		});
+
+		return false;
+	},
+
+	close: function () {
+		// body...
+	},
+
+	destroy: function () {
+		// body...
+	},
+
+	create: function () {
+		// body...
+	}
+};
+
+function anchorLightbox (anchor, options) {
+	var lb = Object.create(lightbox);
+	lb.url = anchor.href;
+	lb.title = anchor.title || anchor.href;
+	Object.extend(lb, options);
+	anchor.onclick = lb.open;
+
+	return lb;
+}
