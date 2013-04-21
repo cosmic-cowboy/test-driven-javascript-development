661b4b4b12aa9689b60c33efc6e89128de9d25aa
hiroo takaku <slgerkamp@gmail.com>
2013-04-21 17:03:34 +0900

    list6-16

--- 06-applied-functions-and-closures/src/tdd.js
+++ 06-applied-functions-and-closures/src/tdd.js
@@ -4,6 +4,7 @@ var tddjs = (function () {
 	function namespace(string){
 		var object = this;
 		var levels = string.split(".");
+
 		for (var i = 0, l = levels.length; i < l; i++) {
 			if(typeof object[levels[i]] == "undefined"){
 				object[levels[i]] = {};
