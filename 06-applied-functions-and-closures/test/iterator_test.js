76133e5708c3f936754cc7b72b414d5e8285922f
hiroo takaku <slgerkamp@gmail.com>
2013-04-21 23:15:44 +0900

    list6-24

--- 06-applied-functions-and-closures/test/iterator_test.js
+++ 06-applied-functions-and-closures/test/iterator_test.js
@@ -3,27 +3,27 @@
 TestCase("IteratorTest",{
 	"test next should return first item": function () {
 		var collection = [1, 2, 3, 4, 5];
-		var iterator = tddjs.iterator(collection);
+		var next = tddjs.iterator(collection);
 
-		assertSame(collection[0], iterator.next());
-		assertTrue(iterator.hasNext());
+		assertSame(collection[0], next());
+		assertTrue(next.hasNext);
 	},
 
 	"test hasNext should be false after last time" : function () {
 		var collection = [1, 2];
-		var iterator   = tddjs.iterator(collection);
-		iterator.next();
-		iterator.next();
-		assertFalse(iterator.hasNext());
+		var next = tddjs.iterator(collection);
+		next();
+		next();
+		assertFalse(next.hasNext);
 	},
 
 	"test should loop collection with iterator" : function () {
 		var collection = [1, 2, 3, 4, 5];
-		var it = tddjs.iterator(collection);
+		var next = tddjs.iterator(collection);
 		var result = [];
 
-		while(it.hasNext()){
-			result.push(it.next());
+		while(next.hasNext){
+			result.push(next());
 		}
 
 		assertEquals(collection, result);
