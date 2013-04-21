a59f1a277b5c98d9f804ebfeb92f4cfcbd18bd23
hiroo takaku <slgerkamp@gmail.com>
2013-04-21 23:35:33 +0900

    list6-26

--- 06-applied-functions-and-closures/src/fibonacci.js
+++ 06-applied-functions-and-closures/src/fibonacci.js
@@ -1,10 +1,16 @@
-// list6-25 フィボナッチ数列
+// list6-26 フィボナッチ数列
 
-function fibonacci (x) {
-
-	if(x < 2){
-		return 1;
+var fibonacci = (function () {
+	var cache = {};
+	function fibonacci (x) {
+		if(x < 2){
+			return 1;
+		}
+		if(!cache[x]){
+			cache[x] = fibonacci(x-1) + fibonacci(x-2);
+		}
+		return cache[x];
 	}
 
-	return fibonacci(x - 1) + fibonacci(x - 2);
-}
+	return fibonacci;
+}());
