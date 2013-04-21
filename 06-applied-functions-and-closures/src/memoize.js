e4d63344a00c4c87ed9504eac142d852a415760c
hiroo takaku <slgerkamp@gmail.com>
2013-04-22 00:05:47 +0900

    list6-29

--- 06-applied-functions-and-closures/src/memoize.js
+++ 06-applied-functions-and-closures/src/memoize.js
@@ -5,10 +5,12 @@ if(!Function.prototype.memoize){
 	Function.prototype.memoize = function() {
 		var cache = {};
 		var func  = this;
+		var join = Array.prototype.join;
 
 		return function (x) {
+			var key = join.call(arguments);
 			if(!(x in cache)){
-				cache[x] = func.call(this, x);
+				cache[key] = func.apply(this, arguments);
 			}
 
 			return cache[x];
