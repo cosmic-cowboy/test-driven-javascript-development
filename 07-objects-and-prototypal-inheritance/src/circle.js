d4719d2db9e04f8227a7995f800c4814093fddeb
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 18:50:41 +0900

    list 7-24

--- 07-objects-and-prototypal-inheritance/src/circle.js
+++ 07-objects-and-prototypal-inheritance/src/circle.js
@@ -1,8 +1,31 @@
 // list 7-13 コンストラクタでオブジェクトを作成する
+// list 7-24 コンストラクタの誤用を検出する
 
 function Circle (radius) {
+
+	if(!(this instanceof Circle)){
+		return new Circle(radius);
+	}
 	this.radius = radius;
 }
+
+
+// list 7-22 constructorプロパティがつくられない問題をクロージャで解決
+
+(function (p) {
+	p.diameter = function() {
+		return this.radius * 2;
+	};
+
+	p.circumference = function() {
+		return this.diameter * Math.PI;
+	};
+
+	p.area = function() {
+		return this.radius * this.radius * Math.PI;
+	};
+}(Circle.prototype));
+
 
 // // list 7-16 Circle.prototypeへのプロパティの追加
 // Circle.prototype.diameter = function() {
@@ -34,20 +57,3 @@ function Circle (radius) {
 // 		return this.radius * this.radius * Math.PI;
 // 	}
 // };
-
-
-// list 7-22 constructorプロパティがつくられない問題をクロージャで解決
-
-(function (p) {
-	p.diameter = function() {
-		return this.radius * 2;
-	};
-
-	p.circumference = function() {
-		return this.diameter * Math.PI;
-	};
-
-	p.area = function() {
-		return this.radius * this.radius * Math.PI;
-	};
-}(Circle.prototype));
