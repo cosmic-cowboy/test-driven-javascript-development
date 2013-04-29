4e8dca56f96cd890ce0cc1d224bc4d8bc5bbdcea
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 17:51:17 +0900

    list 7-20

--- 07-objects-and-prototypal-inheritance/test/circle_test.js
+++ 07-objects-and-prototypal-inheritance/test/circle_test.js
@@ -1,41 +1,48 @@
 // list 7-14 オブジェクトの身元調査
 
 TestCase("CircleTest",{
-	"test inspect objects" : function () {
+	// "test inspect objects" : function () {
 
-		// 円オブジェクトを作成する
-		// Object.prototypeから継承したプロパティ
-		// radiusプロパティを持つ
+	// 	// 円オブジェクトを作成する
+	// 	// Object.prototypeから継承したプロパティ
+	// 	// radiusプロパティを持つ
 
-		var circ = new Circle(6);
-		var circ2 = {radius : 6};
+	// 	var circ = new Circle(6);
+	// 	var circ2 = {radius : 6};
 
 
-		assertTrue(circ instanceof Object);
-		assertTrue(circ instanceof Circle);
-		assertTrue(circ2 instanceof Object);
-		assertEquals(Circle, circ.constructor);
-		assertEquals(Object, circ2.constructor);
-	},
+	// 	assertTrue(circ instanceof Object);
+	// 	assertTrue(circ instanceof Circle);
+	// 	assertTrue(circ2 instanceof Object);
+	// 	assertEquals(Circle, circ.constructor);
+	// 	assertEquals(Object, circ2.constructor);
+	// },
 
-	// list 7-15 同じ種類のオブジェクトの作成
-	// [[prototype]]内部プロパティについて
+	// // list 7-15 同じ種類のオブジェクトの作成
+	// // [[prototype]]内部プロパティについて
 
-	"test should create another object of same kind": function () {
-		var circle = new Circle(6);
-		var circle2 = new circle.constructor(9);
+	// "test should create another object of same kind": function () {
+	// 	var circle = new Circle(6);
+	// 	var circle2 = new circle.constructor(9);
 
-		assertEquals(circle.constructor, circle2.constructor);
-		assertTrue(circle  instanceof Circle);
-		assertTrue(circle2 instanceof Circle);
-	},
+	// 	assertEquals(circle.constructor, circle2.constructor);
+	// 	assertTrue(circle  instanceof Circle);
+	// 	assertTrue(circle2 instanceof Circle);
+	// },
 
-	// list 7-17 Circle.prototype.diameterのテスト
-	"test should inherit properties from Circle prototype" : function () {
-		var circle = new Circle(6);
+	// // list 7-17 Circle.prototype.diameterのテスト
+	// "test should inherit properties from Circle prototype" : function () {
+	// 	var circle = new Circle(6);
 
-		assertEquals(12, circle.diameter());
+	// 	assertEquals(12, circle.diameter());
+	// },
+
+	// list 7-20 constructorプロパティがおかしい
+	"test constructor is Object when prototype is overridden" : function () {
+		function Circle () {}
+		Circle.prototype = {};
+		assertEquals(Object, new Circle().constructor)
+
 	}
-
 
 });
