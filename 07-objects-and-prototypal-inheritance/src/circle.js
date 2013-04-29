bc4a7951d9478769bfc8a8c755b5d0fa3cb6c519
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 17:51:00 +0900

    list 7-18

--- 07-objects-and-prototypal-inheritance/src/circle.js
+++ 07-objects-and-prototypal-inheritance/src/circle.js
@@ -5,15 +5,19 @@ function Circle (radius) {
 }
 
 // list 7-16 Circle.prototypeへのプロパティの追加
+// list 7-18 Circle.prototypeへのプロパティの追加にオブジェクトリテラルを利用
+// prototypeに新しいオブジェクトが代入されたため、constructorプロパティがつくられない
 
-Circle.prototype.diameter = function() {
-	return this.radius * 2;
-};
+Circle.prototype = {
+	diameter : function() {
+		return this.radius * 2;
+	},
 
-Circle.prototype.circumference = function() {
-	return this.diameter * Math.PI;
-};
+	circumference : function() {
+		return this.diameter * Math.PI;
+	},
 
-Circle.prototype.area = function() {
-	return this.radius * this.radius * Math.PI;
+	area : function() {
+		return this.radius * this.radius * Math.PI;
+	}
 };
