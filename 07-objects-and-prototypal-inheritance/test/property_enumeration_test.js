62bfbd1369c57c70f174e987c6b44706c4e5660c
hiroo takaku <slgerkamp@gmail.com>
2013-04-30 07:57:46 +0900

    list 7-29

--- 07-objects-and-prototypal-inheritance/test/property_enumeration_test.js
+++ 07-objects-and-prototypal-inheritance/test/property_enumeration_test.js
@@ -20,22 +20,22 @@ TestCase("PropertyEnumerationTest", {
 			result.push(property);
 		}
 		assertEquals(7, result.length);
-	},
-	"test should enumerate shadowed function properties" : function () {
-		var object = function () {};
-
-		// Function.prototypeでDontEnumが設定されているプロパティ
-		object.prototype = "prototype";
-		object.call = "call";
-		object.apply = "apply";
-
-		var result = [];
-
-		for(var property in object){
-			result.push(property);
-		}
-
-		assertEquals(2, result.length);
-		// assertEquals(3, result.length);
 	}
+	// "test should enumerate shadowed function properties" : function () {
+	// 	var object = function () {};
+
+	// 	// Function.prototypeでDontEnumが設定されているプロパティ
+	// 	object.prototype = "prototype";
+	// 	object.call = "call";
+	// 	object.apply = "apply";
+
+	// 	var result = [];
+
+	// 	for(var property in object){
+	// 		result.push(property);
+	// 	}
+
+	// 	assertEquals(2, result.length);
+	// 	// assertEquals(3, result.length);
+	// }
 });
