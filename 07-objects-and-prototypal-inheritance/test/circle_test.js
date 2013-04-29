d8e3ef4e8b3e464669c98509793d980fc8880b39
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 17:29:34 +0900

    list 7-15

--- 07-objects-and-prototypal-inheritance/test/circle_test.js
+++ 07-objects-and-prototypal-inheritance/test/circle_test.js
@@ -16,5 +16,17 @@ TestCase("CircleTest",{
 		assertTrue(circ2 instanceof Object);
 		assertEquals(Circle, circ.constructor);
 		assertEquals(Object, circ2.constructor);
+	},
+
+	// list 7-15 同じ種類のオブジェクトの作成
+	// [[prototype]]内部プロパティについて
+
+	"test should create another object of same kind": function () {
+		var circle = new Circle(6);
+		var circle2 = new circle.constructor(9);
+
+		assertEquals(circle.constructor, circle2.constructor);
+		assertTrue(circle  instanceof Circle);
+		assertTrue(circle2 instanceof Circle);
 	}
 });
