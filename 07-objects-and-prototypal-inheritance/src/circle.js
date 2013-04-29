c11a9dfe7d7ebfac6ae4a677a6bed25efceae43a
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 17:40:22 +0900

    list 7-16

--- 07-objects-and-prototypal-inheritance/src/circle.js
+++ 07-objects-and-prototypal-inheritance/src/circle.js
@@ -3,3 +3,17 @@
 function Circle (radius) {
 	this.radius = radius;
 }
+
+// list 7-16 Circle.prototypeへのプロパティの追加
+
+Circle.prototype.diameter = function() {
+	return this.radius * 2;
+};
+
+Circle.prototype.circumference = function() {
+	return this.diameter * Math.PI;
+};
+
+Circle.prototype.area = function() {
+	return this.radius * this.radius * Math.PI;
+};
