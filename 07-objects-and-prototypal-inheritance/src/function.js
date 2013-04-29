a76ea4b52a8bc5ed1e6be5aa17b84e1367776bda
hiroo takaku <slgerkamp@gmail.com>
2013-04-30 07:52:55 +0900

    list 7-28

---
+++ 07-objects-and-prototypal-inheritance/src/function.js
@@ -0,0 +1,16 @@
+// list 7-28 inheritの実装
+
+if(!Function.prototype.inherit){
+	(function () {
+
+		// 中間的なコンストラクタ F
+		function F () {}
+
+		Function.prototype.inherit = function(superFn) {
+			F.prototype = superFn.prototype;
+			this.prototype = new F();
+			this.prototype.constructor = this;
+		};
+
+	})();
+}
