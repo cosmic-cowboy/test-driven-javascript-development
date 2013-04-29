640708ce1b5645885d6ddea8b8a4c4df9fb87c1e
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 12:29:33 +0900

    list 7-1

---
+++ 07-objects-and-prototypal-inheritance/src/car.js
@@ -0,0 +1,20 @@
+// list7-1 オブジェクトリテラル
+
+var car = {
+	model : {
+		year : "1998",
+		make : "Ford",
+		model: "Mondeo"
+	},
+
+	color : "Red",
+	seats : 5,
+	doors : 5,
+	accessories : [
+		"Air condition",
+		"Electric Windows"
+	],
+	drive : function () {
+		console.log("Vroooom!");
+	}
+};
