6a507bd4a0f9ccdc343f2d93165665e9c54d8bbc
hiroo takaku <slgerkamp@gmail.com>
2013-04-29 17:56:04 +0900

    list 7-21

--- 07-objects-and-prototypal-inheritance/src/circle.js
+++ 07-objects-and-prototypal-inheritance/src/circle.js
@@ -4,11 +4,25 @@ function Circle (radius) {
 	this.radius = radius;
 }
 
-// list 7-16 Circle.prototypeへのプロパティの追加
+// // list 7-16 Circle.prototypeへのプロパティの追加
+// Circle.prototype.diameter = function() {
+// 	return this.radius * 2;
+// };
+
+// Circle.prototype.circumference = function() {
+// 	return this.diameter * Math.PI;
+// };
+
+// Circle.prototype.area = function() {
+// 	return this.radius * this.radius * Math.PI;
+// };
+
 // list 7-18 Circle.prototypeへのプロパティの追加にオブジェクトリテラルを利用
 // prototypeに新しいオブジェクトが代入されたため、constructorプロパティがつくられない
+// list 7-21 constructorプロパティがつくられない問題を原始的に解決（解決といえるのか？）
 
 Circle.prototype = {
+	constructor : Circle,
 	diameter : function() {
 		return this.radius * 2;
 	},
