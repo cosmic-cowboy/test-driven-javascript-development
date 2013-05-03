ad10c060382bf34f64dea3c1b16f2928e6ac924c
hiroo takaku <slgerkamp@gmail.com>
2013-05-03 17:42:14 +0900

    list 7-33

--- 07-objects-and-prototypal-inheritance/test/sphere_test.js
+++ 07-objects-and-prototypal-inheritance/test/sphere_test.js
@@ -17,5 +17,14 @@ TestCase("SphereTest", {
 		var sphere = new Sphere(3);
 
 		assertEquals(113, Math.round(sphere.area()));
+	},
+
+	// lsit 7-33 _superがプロトタイプを参照するという前提でのテスト
+	"test should set up link to super" : function () {
+		var SubFn   = function () {};
+		var SuperFn = function () {};
+		SubFn.inherit(SuperFn);
+
+		assertEquals(SuperFn.prototype, SubFn.prototype._super);
 	}
 });
