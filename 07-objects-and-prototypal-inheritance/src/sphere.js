02ea636cc27ed86cff831c324919a60e6544e476
hiroo takaku <slgerkamp@gmail.com>
2013-05-03 17:18:19 +0900

    list 7-30

--- 07-objects-and-prototypal-inheritance/src/sphere.js
+++ 07-objects-and-prototypal-inheritance/src/sphere.js
@@ -1,11 +1,20 @@
-// list 7-25 深い継承
+// list 7-30 Sphereコンストラクタの中からCircleコンストラクタにアクセス
 
 function Sphere (radius) {
-	this.radius = radius;
+	Circle.call(this, radius);
 }
 
 // 7-29 inheritを使ってSphereにCircleを継承させる
+
 Sphere.inherit(Circle);
+
+
+// list 7-25 深い継承
+
+// function Sphere (radius) {
+// 	this.radius = radius;
+// }
+
 
 // Sphere.prototype = (function() {
 // 	function F (){}
