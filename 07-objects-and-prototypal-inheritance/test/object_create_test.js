69d028a0a8cf1445ffc1a2ddcb5c3bf4e6a2d147
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 06:48:31 +0900

    list 7-48

--- 07-objects-and-prototypal-inheritance/test/object_create_test.js
+++ 07-objects-and-prototypal-inheritance/test/object_create_test.js
@@ -17,5 +17,19 @@ TestCase("ObjectCreateTest",{
 		};
 
 		assertEquals(452, Math.round(sphere.area()));
+	},
+
+	"test should create more spheres based on existing" : function () {
+		var circle = new Circle(6);
+		var sphere = Object.create(circle);
+
+		sphere.area = function () {
+			return 4 * circle.area.call(this);
+		};
+
+		var sphere2 = Object.create(sphere);
+		sphere2.radius = 10;
+
+		assertEquals(1257, Math.round(sphere2.area()));
 	}
 });
