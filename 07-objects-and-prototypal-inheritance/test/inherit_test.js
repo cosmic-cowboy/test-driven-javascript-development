d4f4cb48a7bddb31252db4ff6f34e24a1a2566ea
hiroo takaku <slgerkamp@gmail.com>
2013-05-04 15:52:07 +0900

    list7-40

--- 07-objects-and-prototypal-inheritance/test/inherit_test.js
+++ 07-objects-and-prototypal-inheritance/test/inherit_test.js
@@ -9,12 +9,35 @@ TestCase("FunctionInheritTest",{
 		assertTrue(new SubFn() instanceof SuperFn);
 	},
 
-	// lsit 7-33 _superがプロトタイプを参照するという前提でのテスト
+	// list 7-33 _superがプロトタイプを参照するという前提でのテスト
 	"test should set up link to super" : function () {
 		var SubFn   = function () {};
 		var SuperFn = function () {};
 		SubFn.inherit(SuperFn);
 
 		assertEquals(SuperFn.prototype, SubFn.prototype._super);
+	},
+
+	// list 7-40 より単純な_superヘルパーの使い方
+	"test should access super method with helper": function () {
+
+		function LoudPerson (name) {
+			_super(this, "constructor", name);
+
+			LoudPerson.inherit(Person);
+
+			LoudPerson.prototype.getName = function() {
+				return _super(this, "getName").toUpperCase();
+			};
+
+			LoudPerson.prototype.say = function(words) {
+				return _super(this, "speak", words) + "!!!";
+			};
+
+			var np = new LoudPerson("Chris");
+
+			assertEquals("CHRIS", np.getName());
+			assertEquals("Hello!!!", np.say("Hello"));
+		}
 	}
 });
