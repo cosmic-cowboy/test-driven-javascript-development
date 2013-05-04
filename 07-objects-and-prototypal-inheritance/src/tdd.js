3ddbfeecd2cd1fba18bd49d706f186cfe9776ff6
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 08:32:36 +0900

    list7-57

--- 07-objects-and-prototypal-inheritance/src/tdd.js
+++ 07-objects-and-prototypal-inheritance/src/tdd.js
@@ -147,10 +147,15 @@ tddjs.each = (function () {
 
 // list 7-53 tddjs.extendの初期実装
 // list 7-55 targetがnullになっているのを認める（許容する）
+// list 7-57 sourceがなければ異常終了
 
 tddjs.extend = (function () {
   function extend (target, source) {
     target = target || {};
+
+    if(!source){
+      return target;
+    }
 
     tddjs.each(source, function (prop, val) {
       target[prop] = val;
