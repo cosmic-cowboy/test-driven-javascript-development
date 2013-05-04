846cbb47f5f92b529725e085010a0d09c163f9b4
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 07:27:11 +0900

    list 7-51

--- 07-objects-and-prototypal-inheritance/test/arguments_test.js
+++ 07-objects-and-prototypal-inheritance/test/arguments_test.js
@@ -13,6 +13,23 @@ TestCase("ArgumentsTest", {
 		var result = addToArray([], 1, 2, 3);
 
 		assertEquals([1, 2, 3], result);
+    },
+
+    // list 7-51 明示的な借用
+
+    "test arguments should borrow explicitly from Array.prototype" : function () {
+
+		function addToArray() {
+			arguments.slice = Array.prototype.slice;
+			var args = arguments.slice();
+			var arr = args.shift();
+
+			return arr.concat(args);
+		}
+
+		var result = addToArray([], 1, 2, 3);
+
+		assertEquals([1, 2, 3], result);
     }
 });
 
