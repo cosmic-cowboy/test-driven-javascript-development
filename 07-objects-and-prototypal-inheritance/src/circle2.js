c4d037b33d3f0231bbe87345fe88062923a78513
hiroo takaku <slgerkamp@gmail.com>
2013-05-04 16:50:44 +0900

    list 7-42

--- 07-objects-and-prototypal-inheritance/src/circle2.js
+++ 07-objects-and-prototypal-inheritance/src/circle2.js
@@ -1,12 +1,8 @@
-// list 7-41 非公開関数の定義
+
+// list 7-42 コンストラクタ内部の非公開関数の使い方
 
 function Circle2 (radius) {
 	this.radius = radius;
-}
-
-(function (circleProto) {
-
-	// スコープ内で宣言された関数は非公開
 
 	function ensureValidRadius (radius) {
 		return radius >= 0;
@@ -22,9 +18,39 @@ function Circle2 (radius) {
 		}
 	}
 
-	// プロトタイプのプロパティに関数を代入すると公開メソッドになる
+	// 公開メソッドを外から見えるように
+	this.getRadius = getRadius;
+	this.setRadius = setRadius;
+}
 
-	circleProto.getRadius = getRadius;
-	circleProto.setRadius = setRadius;
 
-})(Circle2.prototype);
+// // list 7-41 非公開関数の定義
+
+// function Circle2 (radius) {
+// 	this.radius = radius;
+// }
+
+// (function (circleProto) {
+
+// 	// スコープ内で宣言された関数は非公開
+
+// 	function ensureValidRadius (radius) {
+// 		return radius >= 0;
+// 	}
+
+// 	function getRadius() {
+// 		return this.radius;
+// 	}
+
+// 	function setRadius(radius) {
+// 		if(ensureValidRadius(radius)){
+// 			this.radius = radius;
+// 		}
+// 	}
+
+// 	// プロトタイプのプロパティに関数を代入すると公開メソッドになる
+
+// 	circleProto.getRadius = getRadius;
+// 	circleProto.setRadius = setRadius;
+
+// })(Circle2.prototype);
