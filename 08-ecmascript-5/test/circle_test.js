0e0be60d45fec20d6e58c330b834fefc2b028007
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 13:19:37 +0900

    list 8-12

---
+++ 08-ecmascript-5/test/circle_test.js
@@ -0,0 +1,15 @@
+TestCase("CircleTest", {
+	// list 8-12 ハイブリッドCircleの使い方
+	"test Object.create backed constructor" : function () {
+		var circle = new Circle(3);
+
+		assert(circle instanceof Circle);
+		assertEquals(6, circle.diameter);
+
+		circle.radius = 6;
+		assertEquals(12, circle.diameter);
+
+		delete circle.radius;
+		assertEquals(6, circle.radius);
+	}
+});
