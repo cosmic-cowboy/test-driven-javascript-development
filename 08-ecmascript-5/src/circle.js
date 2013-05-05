087833e6e3b4c626c02ba3ec33e84e15ea11f991
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 13:16:54 +0900

    list 8-11

--- 08-ecmascript-5/src/circle.js
+++ 08-ecmascript-5/src/circle.js
@@ -1,5 +1,51 @@
-// list 8-1 単純な名前/値を代入する
+// list 8-11 Object.create を使ったコンストラクタ
 
-var circle = {};
+function Circle (radius) {
+	var _radius;
 
-circle.radius = 4;
+	var circle = Object.create(
+		Circle.prototype, {
+			radius : {
+				configurable : false,
+				enumerable : true,
+
+				set : function (r) {
+					if(typeof r != "number" || r <= 0){
+						throw new TypeError("radius should be > 0");
+					}
+
+					_radius = r;
+				},
+
+				get : function () {
+					return _radius;
+				}
+			}
+		}
+	);
+
+	circle.radius = radius;
+
+	return circle;
+}
+
+Circle.prototype = Object.create(Circle.prototype, {
+
+	diameter : {
+		get : function () {
+			return this.radius * 2;
+		},
+		configurable : false,
+		enumerable : true
+	},
+
+	circumference : {},
+	area : {}
+});
+
+
+// // list 8-1 単純な名前/値を代入する
+
+// var circle = {};
+
+// circle.radius = 4;
