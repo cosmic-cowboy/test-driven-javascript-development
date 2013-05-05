cf110140220d57b58136f07ae7a01dbceadf6301
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 14:23:28 +0900

    list 8-20

--- 08-ecmascript-5/test/strict_mode_test.js
+++ 08-ecmascript-5/test/strict_mode_test.js
@@ -22,3 +22,24 @@ TestCase("StrictModeTest",{
 		assertEquals(6, es3VsEs5(2,3,6));
 	}
 });
+
+
+// list 8-20 
+function switchArgs (a, b) {
+	"use strict";
+	var c = b;
+	b = a;
+	a = c;
+
+	return [].slice.call(arguments);
+}
+
+TestCase("ArgumentsParametersTest", {
+	"test should switch arguments" : function () {
+		// ES5 厳密モード
+		assertEquals([2,3], switchArgs(2,3));
+
+		// // ES3モード
+		// assertEquals([3,2], switchArgs(2,3));
+	}
+});
