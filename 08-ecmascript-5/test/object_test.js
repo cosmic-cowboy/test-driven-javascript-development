01f26a3721901066f92deefb21130e1af84b48bd
hiroo takaku <slgerkamp@gmail.com>
2013-05-05 10:38:43 +0900

    list8-3

--- 08-ecmascript-5/test/object_test.js
+++ 08-ecmascript-5/test/object_test.js
@@ -11,5 +11,22 @@ TestCase("ES5ObjectTest", {
 		});
 
 		assertEquals(4, circle.radius);
+	},
+
+	// list 8-3 プロパティデスクリプターの変更
+	// enumerable:for-inループでプロパティを反復処理できるか
+	// configurable:delete演算子でプロパティを削除できるか
+	// writable:プロパティを上書きできるか
+
+	"test changing a propety descriptor" : function () {
+		var circle = {radius : 3};
+		var descriptor = Object.getOwnPropertyDescriptor(circle, "radius");
+		descriptor.configurable = false;
+
+		Object.defineProperty(circle, "radius", descriptor);
+		delete circle.radius;
+
+		// configurableでないradiusは削除できない
+		assertEquals(3, circle.radius);
 	}
 });
