869fa16375062baa8147c877004a1e70dab15fe9
hiroo takaku <slgerkamp@gmail.com>
2013-05-06 16:45:00 +0900

    list 9-13 revised

--- 09-unobtrusive-javascript/unobtrusive/jqtabs.js
+++ 09-unobtrusive-javascript/unobtrusive/jqtabs.js
@@ -2,15 +2,21 @@
 
 (function ($) {
 	$.fn.tabs = function () {
-		$this = $(this);
-		$this.addClass("js-tabs").find("ol:first a").live("click", function (event) {
-			$target = $(event.target);
-			$target.parents("ol").find("a").removeClass("active-tab");
-			$target.addClass("active-tab");
-
-			var activehash = $target.attr("href").replace(/^.*#/,"");
-			$activePanel = $("[name=" + activehash + "]");
-			$activePanel.parents("div").addClass("active-panel").siblings("div.section").removeClass("active-panel");
+		$anchors = $(this).addClass("js-tabs").find("ol:first a");
+		$anchors.live("click", function (event) {
+			tabEvent($(event.target));
 		});
+
+		tabEvent($anchors.slice(0,1));
 	};
+
+	function tabEvent($target){
+		$target.parents("ol").find("a").removeClass("active-tab");
+		$target.addClass("active-tab");
+
+		var activehash = $target.attr("href").replace(/^.*#/,"");
+		$activePanel = $("[name=" + activehash + "]");
+		$activePanel.parents("div").addClass("active-panel").siblings("div.section").removeClass("active-panel");
+	}
+
 }(jQuery));
