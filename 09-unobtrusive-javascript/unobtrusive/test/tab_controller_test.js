ef1280fece6bc9926fe9d0a862f882dd0e6ae743
hiroo takaku <slgerkamp@gmail.com>
2013-05-06 15:25:33 +0900

    list 9-8

--- 09-unobtrusive-javascript/unobtrusive/test/tab_controller_test.js
+++ 09-unobtrusive-javascript/unobtrusive/test/tab_controller_test.js
@@ -38,4 +38,58 @@
 
 	});
 
+	// list 9-8 activateTabメソッドを対象とするテストケース
+	TestCase("TabbedControllerActivateTabTest", {
+		setUp : function  () {
+			setUp.call(this);
+			this.controller = tabController.create(this.tabs);
+			this.links = this.tabs.getElementByTagName("a");
+			this.lis = this.tabs.getElementByTagName("li");
+		},
+
+		"test should not fail without anchor" : function () {
+			var controller = this.controller;
+
+			assertException(function () {
+				controller.activateTab();
+			});
+		},
+
+		"test should mark anchor as active" : function () {
+			this.controller.activateTab(this.links[0]);
+
+			assertClassName("activate-tab", this.links[0]);
+
+		},
+
+		"test should deactivate previous tab" : function () {
+			this.controller.activateTab(this.links[0]);
+			this.controller.activateTab(this.links[1]);
+
+			assertNoMatch(/(^|\s)activate-tab(\s|$)/, this.links[0]);
+			assertClassName("activate-tab", this.links[1]);
+		},
+
+		"test should not activate unsupported element type": function () {
+			this.controller.activateTab(this.links[0]);
+			this.controller.activateTab(this.lis[0]);
+
+			assertNoMatch(/(^|\s)activate-tab(\s|$)/, this.lis[0]);
+			assertClassName("activate-tab", this.links[0]);
+		},
+
+		"test should fire onTabChange": function () {
+			var actualPrevious, actualCurrent;
+			this.controller.activateTab(this.links[0]);
+			this.controller.onTabChange = function (curr, prev) {
+				actualPrevious = prev;
+				actualCurrent = curr;
+			};
+
+			this.controller.activateTab(this.links[1]);
+
+			assertSame(actualPrevious, this.links[0]);
+			assertSame(actualCurrent, this.links[1]);
+		}
+	});
 }());
