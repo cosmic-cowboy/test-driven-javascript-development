25e95847d18f60f3eb9729e4bd62416a408e8a02
hiroo takaku <slgerkamp@gmail.com>
2013-05-07 07:02:01 +0900

    list 10-2

--- 10-feature-detection/src/events.js
+++ 10-feature-detection/src/events.js
@@ -1,10 +1,9 @@
-// list 10-1 ブラウザの推測を使ってイベントリスニングの違いを吸収する
+// list 10-2 オブジェクトの検出を使ったブラウザの推測
 // 悪い例
-// listenerの引数としてイベントを渡し、thisの値を修正する
-// IEはグローバルオブジェクトをthisとしてlistenerを呼び出す
+// IEがグローバルなActiveXObjectを提供していることを利用
 
 function addEventHandler (element, type, listener) {
-	if(/MSIE/.test(navigator.userAgent)){
+	if(window.ActiveXObject){
 		element.attachEvent("on" + type, function () {
 			return listener.call(element, window.event);
 		});
