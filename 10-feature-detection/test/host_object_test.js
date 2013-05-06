88d814952d77cd2c45b0f4c0d97660f45aa4ab97
hiroo takaku <slgerkamp@gmail.com>
2013-05-07 07:49:18 +0900

    list 10-6

--- 10-feature-detection/test/host_object_test.js
+++ 10-feature-detection/test/host_object_test.js
@@ -5,5 +5,24 @@ TeseCase("HostObjectTest",{
 		// list 10-5 Internet Exploreでのtypeofとホストオブジェクト
 		assertEquals("object", typeof document.attachEvent);
 
+		// list 10-6 フレンドリではないホストオブジェクトのふるまい
+
+		var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+
+		assertException(function () {
+			if(xhr.open){
+				// プロパティはあるはずだが、例外が投げられる
+				// ホストオブジェクトのため...
+			}
+		});
+
+		assertEquals("unknown", typeof xhr.open);
+
+		var element = document.createElement("div");
+		assertEquals("unknown", typeof element.offsetParent);
+
+		assertException(function(){
+			var parent = element.offsetParent;
+		});
 	}
 });
