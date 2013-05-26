49f77bf40e48cc7518d522a3b09067b824e6022e
hiroo takaku <slgerkamp@gmail.com>
2013-05-26 17:14:26 +0900

    list 11-13

--- 11-the-observer-pattern/test/observable_test.js
+++ 11-the-observer-pattern/test/observable_test.js
@@ -1,14 +1,16 @@
 // list 11-3 addObserverが内部配列に観察者を追加することを確かめる
+// list 11-13 ハードコードされたソリューションの問題点を暴く
 
 TestCase("ObservableAddObserverTest", {
 	"test should store function" : function () {
 		// 観察対象オブジェクト
 		var observable = new tddjs.util.Observable();
 		// 観察者
-		var observer = function () {};
+		var observers = [function () {}, function () {}];
 
-		observable.addObserver(observer);
+		observable.addObserver(observers[0]);
+		observable.addObserver(observers[1]);
 
-		assertEquals(observer, observable.observers[0]);
+		assertEquals(observers, observable.observers);
 	}
 });
