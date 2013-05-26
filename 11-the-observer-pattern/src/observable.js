0ca13ead130340b4318fa8514bb86adf9cdddbef
hiroo takaku <slgerkamp@gmail.com>
2013-05-26 17:46:26 +0900

    list 11-20

--- 11-the-observer-pattern/src/observable.js
+++ 11-the-observer-pattern/src/observable.js
@@ -22,9 +22,16 @@ tddjs.namespace('util');
 
 	// list 11-16 hasObserverからの応答をハードコードする
 	// list 11-18 観察者がいるかどうかを実際にチェックする
+	// list 11-20 手作業で配列をループで処理する
 	function hasObserver (observer) {
-		return this.observers.indexOf(observer) >= 0;
+		for (var i = 0, l = this.observers.length; i < l; i++){
+			if(this.observers[i] === observer){
+				return true;
+			}
+		}
+		return false;
 	}
+
 	Observable.prototype.hasObserver = hasObserver;
 
 }());
