841d4db0156fa8656825ac583dffd677caf4d1b8
hiroo takaku <slgerkamp@gmail.com>
2013-05-26 17:14:43 +0900

    list 11-14

--- 11-the-observer-pattern/src/observable.js
+++ 11-the-observer-pattern/src/observable.js
@@ -5,15 +5,18 @@ tddjs.namespace('util');
 // list 11-7 コンストラクタを追加する
 
 (function () {
-	
+
 	function Observable () {
+		// list 11-14 配列を正しく追加
+		this.observers = [];
 	}
 	tddjs.util.Observable = Observable;
+
 
 	// list 11-9 addObserverメソッドを追加する
 	// list 11-11 配列をハードコードする
 	function addObserver (observer) {
-		this.observers = [observer]
+		this.observers.push(observer);
 	}
 	Observable.prototype.addObserver = addObserver;
 
