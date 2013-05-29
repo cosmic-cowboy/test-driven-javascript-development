e969aca9a34f2f14922786381f4a5389e2cd0801
hiroo takaku <slgerkamp@gmail.com>
2013-05-30 07:27:22 +0900

    list 11-30

--- 11-the-observer-pattern/test/observable_test.js
+++ 11-the-observer-pattern/test/observable_test.js
@@ -79,6 +79,27 @@ TestCase("ObservableNotifyObserversTest", {
 
 		assertTrue(observer2.called);
 
+	},
+
+	// list 11-30  呼び出し順を機能として保証する
+	"test should call observers in the order they were added" : function () {
+		var observable = new tddjs.util.Observable();
+		var calls = [];
+
+		var observer1 = function () {
+			calls.push(observer1);
+		};
+		var observer2 = function () {
+			calls.push(observer2);
+		};
+
+		observable.addObserver(observer1);
+		observable.addObserver(observer2);
+
+		observable.notifyObservers();
+
+		assertEquals(observer1, calls[0]);
+		assertEquals(observer2, calls[1]);
 	}
 });
 
