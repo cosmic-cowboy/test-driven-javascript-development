d7b4ca495c65f216b0396aea1f258aa42ca9bc80
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 11:47:52 +0900

    list 12-12 URLが文字列でなければ例外を投げる

--- 12-abstracting-browser-differences-ajax/src/request.js
+++ 12-abstracting-browser-differences-ajax/src/request.js
@@ -2,5 +2,11 @@
 // URLとHTTP同士でサーバーに要求を発行するインターフェイスをつくる
 // 成功時、失敗時のコールバックを指定する
 // list 12-10 tddjs.ajax.getを定義する
+// 12.4.1 URLの要件とする
+// list 12-12 URLが文字列でなければ例外を投げる
 
-tddjs.namespace("ajax").get = function () {};
+tddjs.namespace("ajax").get = function (url) {
+	if(typeof url !== "string"){
+		throw new TypeError("URL should be string");
+	}
+};
