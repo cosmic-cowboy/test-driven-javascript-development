44472c303a2b0d4cfbc54024ef11bd9ef063e855
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 15:31:34 +0900

    list 12-22 スタブ関数作成ヘルパーを改良する（引数を記録する）

--- 12-abstracting-browser-differences-ajax/lib/stub.js
+++ 12-abstracting-browser-differences-ajax/lib/stub.js
@@ -3,11 +3,15 @@
 // list 12-18 スタブ関数作成ヘルパーを抽出する
 // list 12-20 openメソッドが正しく使われていることをテスト
 // 取得したXMLHttpRequestオブジェクトのopenメソッドが呼び出せるようにする
+// list 12-22 スタブ関数作成ヘルパーを改良する（引数を記録する）
+
 function stubFn (returnValue) {
 	var fn = function () {
 		fn.called = true;
+		fn.args = arguments;
 		return returnValue;
 	};
+
 	fn.called = false;
 
 	return fn;
