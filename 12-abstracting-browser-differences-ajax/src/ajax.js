fb47882232016fd5568bb7b03a326ad738b8902f
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 11:10:57 +0900

    list 12-7 あらかじめサポートされているかどうかをチェックする（即時関数を利用して、ロード時に一回だけ実行されるようにする）

--- 12-abstracting-browser-differences-ajax/src/ajax.js
+++ 12-abstracting-browser-differences-ajax/src/ajax.js
@@ -2,8 +2,17 @@
 // list 12-4 ajax名前空間を作る
 // 12.3.3 createを実装する
 // list 12-6 XMLHttpRequestオブジェクトをつくる
+//（オブジェクトがないとexceptionが発生する）
+// list 12-7 あらかじめサポートされているかどうかをチェックする
+//（即時関数を利用して、ロード時に一回だけ実行されるようにする）
 
-tddjs.namespace("ajax").create = function(){
+
+(function () {
+	var xhr;
+	var ajax = tddjs.namespace("ajax");
+
+
+
 	var options = [
 		function () {
 			return new ActiveXObject("Microsoft.XMLHTTP");
@@ -15,10 +24,12 @@ tddjs.namespace("ajax").create = function(){
 
 	for (var i = 0; i < options.length; i++) {
 		try{
-			return options[i]();
+			// 実行できると、createに代入、実行できないとexception
+			xhr = options[i]();
+			ajax.create = options[i];
+			break;
 		} catch(e){
 			console.log("error:" + options[i]);
 		}
 	}
-	return null;
-};
+}());
