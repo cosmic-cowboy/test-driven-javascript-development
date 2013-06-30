a3766ee136dfc079da04fb1e648ab13dae0e34ab
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 17:14:47 +0900

    list 12-30 スタブのsendメソッドを追加する

--- 12-abstracting-browser-differences-ajax/lib/fake_xhr.js
+++ 12-abstracting-browser-differences-ajax/lib/fake_xhr.js
@@ -1,6 +1,7 @@
 // list 12-24 fakeXMLHtppRequestを独立させる
 // ダミーのXMLHttpRequestを複数のテストで共有できるように外出しする
-
+// list 12-30 スタブのsendメソッドを追加する
 var fakeXMLHtppRequest = {
-	open : stubFn()
+	open : stubFn(),
+	send : stubFn()
 };
