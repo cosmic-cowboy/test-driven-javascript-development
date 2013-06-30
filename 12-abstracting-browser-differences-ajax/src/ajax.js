2007b9394ec8730c5559adced8a1ea21aed5fe91
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 11:03:19 +0900

    list 12-6 XMLHttpRequestオブジェクトをつくる

--- 12-abstracting-browser-differences-ajax/src/ajax.js
+++ 12-abstracting-browser-differences-ajax/src/ajax.js
@@ -1,4 +1,24 @@
 // 12.3 XMLHttpRequestオブジェクトをつくる
 // list 12-4 ajax名前空間を作る
+// 12.3.3 createを実装する
+// list 12-6 XMLHttpRequestオブジェクトをつくる
 
-tddjs.namespace("ajax");
+tddjs.namespace("ajax").create = function(){
+	var options = [
+		function () {
+			return new ActiveXObject("Microsoft.XMLHTTP");
+		},
+		function () {
+			return new XMLHttpRequest();
+		}
+	];
+
+	for (var i = 0; i < options.length; i++) {
+		try{
+			return options[i]();
+		} catch(e){
+			console.log("error:" + options[i]);
+		}
+	}
+	return null;
+};
