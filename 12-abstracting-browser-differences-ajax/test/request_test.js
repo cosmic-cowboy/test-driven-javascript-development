ab7b74d12591b132c440fb79fb09a4a8eb9f7915
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 11:47:34 +0900

    list 12-11 URLが必須とされていることをテストする

--- 12-abstracting-browser-differences-ajax/test/request_test.js
+++ 12-abstracting-browser-differences-ajax/test/request_test.js
@@ -2,9 +2,16 @@
 // URLとHTTP同士でサーバーに要求を発行するインターフェイスをつくる
 // 成功時、失敗時のコールバックを指定する
 // list 12-9 tddjs.ajax.getが定義されていることを確認
+// 12.4.1 URLの要件とする
+// list 12-11 URLが必須とされていることをテストする
 
 TestCase("GetRequestTest", {
 	"test should define get method" : function () {
 		assertFunction(tddjs.ajax.get);
+	},
+	"test should throw error without url" : function () {
+		assertException(function () {
+			tddjs.ajax.get();
+		}, "TypeError");
 	}
 });
