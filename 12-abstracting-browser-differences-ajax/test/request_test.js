7ee565a34f298d45d8d6120cd838f4e9a7c86b25
hiroo takaku <slgerkamp@gmail.com>
2013-06-30 13:34:08 +0900

    list 12-15 createメソッドを手作業でスタブに置き換える（ajax.createをスタブに置き換えている）

--- 12-abstracting-browser-differences-ajax/test/request_test.js
+++ 12-abstracting-browser-differences-ajax/test/request_test.js
@@ -5,7 +5,8 @@
 // 12.4.1 URLの要件とする
 // list 12-11 URLが必須とされていることをテストする
 // list 12-13 テストにajax名前空間を「インポート」する
-
+// list 12-15 createメソッドを手作業でスタブに置き換える
+//（ajax.createをスタブに置き換えている）
 (function (){
 
 	var ajax = tddjs.ajax;
@@ -18,6 +19,19 @@
 			assertException(function () {
 				ajax.get();
 			}, "TypeError");
+		},
+		"test should obtain an XMLHttpRequest object" : function () {
+			// 元のメソッドの参照を保存
+			var originalCreate = ajax.create;
+			// スタブ関数で上書き
+			ajax.create = function () {
+				ajax.create.called = true;
+			};
+			ajax.get("/url");
+
+			assert(ajax.create.called);
+			// 元のメソッドを復元
+			ajax.create = originalCreate;
 		}
 	});
 
